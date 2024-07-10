7743cb0b0d3a990b8ad87a0db55714d6bb990a36
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2024-07-10 21:27:39 +0530

    Loadash done

--- server.js
+++ server.js
@@ -1,55 +1,67 @@
-// var add = function(a,b){
-//     return a + b;
-// }
-// var add = add(5,3);
-// console.log(add);
+//? var add = function(a,b){
+//?     return a + b;
+//? }
+//?? var add = add(5,3);
+//?? console.log(add);
 
-// var add = (a,b) =>{
-//     return a + b;
-// }
-// var add = add(5,5);
-// console.log(add);
+//?? var add = (a,b) =>{
+//?     return a + b;
+//? }
+//? var add = add(5,5);
+//? console.log(add);
 
-// Callback Functions --------------------------------
-// const prince = function(){
-//     console.log("Hii i am callback");
-// } 
-// const add = function(a,b,prince){
-//  var result = a+b;
-//  console.log(" Result is:"+result);
-//  prince();
-// }
-// add(5,5,prince);
-
-
-
-// const add = function(a,b,callback){
-//     var result = a+b;
-//     console.log(" Result is:"+result);
-//     callback();
-//    }
-//    add(5,5,function(){console.log("Hii i am also callback")} );
+//? Callback Functions --------------------------------
+//? const prince = function(){
+//?     console.log("Hii i am callback");
+//? } 
+//? const add = function(a,b,prince){
+//?  var result = a+b;
+//?  console.log(" Result is:"+result);
+//?  prince();
+//? }
+//? add(5,5,prince);
 
 
 
-// Core module fs/os
-// var fs = require("fs");
-// var os = require("os");
-
-// var user = os.userInfo();
-// console.log(user);
-// console.log(user.username);
-
-// fs.appendFile("greeting.txt", "Hi "+user.username+"\n", function(){
-//     console.log("File is created successfully🤑");
-// });
+//? const add = function(a,b,callback){
+//?     var result = a+b;
+//?     console.log(" Result is:"+result);
+//?     callback();
+//?    }
+//?    add(5,5,function(){console.log("Hii i am also callback")} );
 
 
-// By this style we can import our file and function  from separate folder and path
-const notes = require("./note.js");
 
-var age =  notes.age;
-console.log(age);
+// *Core module fs/os
+//? var fs = require("fs");
+//? var os = require("os");
 
-var result = notes.add(age+12,10);
-console.log(result);
+//? var user = os.userInfo();
+//? console.log(user);
+//? console.log(user.username);
+
+//? fs.appendFile("greeting.txt", "Hi "+user.username+"\n", function(){
+//?     console.log("File is created successfully🤑");
+//? });
+
+
+//* By this style we can import our file and function  from separate folder and path
+//? const notes = require("./note.js");
+
+//? var age =  notes.age;
+//? console.log(age);
+
+//? var result = notes.add(age+12,10);
+//? console.log(result);
+
+// * LOADASH
+var _ = require('lodash');
+
+var data = ["hii", 1,2,1,2,1,3,4,"hii","hlw"];
+var filter = _.uniq(data);
+console.log(filter);
+
+var data = "ds";
+console.log(_.isString(data)); //true
+console.log(_.isString(5)); //false
+console.log(_.isString("hii"));  //true
